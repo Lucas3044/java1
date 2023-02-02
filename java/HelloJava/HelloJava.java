90ee6df13301ab879d1f60d457ebc358adfd872d
Lucas <blackdoc84@gmail.com>
2023-02-02 22:41:50 +0900

    first commit

--- java/HelloJava/HelloJava.java
+++ java/HelloJava/HelloJava.java
@@ -6,5 +6,7 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("hello ~ hi~");
         System.out.println("hello ~~");
+        System.out.println("hello22 ~~");
+
     }
 }
